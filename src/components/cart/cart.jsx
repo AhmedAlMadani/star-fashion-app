d3f1266d19d42213946e9c83264af9ceab7a8f47
Ahmed Madani <ahmedmoeen87@gmail.com>
2022-04-01 00:35:09 +0300

    Cart dropdown finalized, card design and cart item functionality added, cart context updated

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -6,13 +6,13 @@ import { CartContext } from '../../contexts/cart.context';
 import {ReactComponent as CartIcon} from '../../assets/shopping-bag.svg';
 
 const Cart = () => {
-    const { isCartOpen ,setIsCartOpen} = useContext(CartContext);
+    const { isCartOpen ,setIsCartOpen, cartCount} = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
     return(
         <div className='cart-container' onClick={toggleIsCartOpen}>
             <CartIcon className='cart-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 };
